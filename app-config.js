727184fc522f4e32c08418fd4bbed733c2dd8d54
Jakeroberson1 <jake.roberson@outlook.com>
2026-02-19 13:06:36 -0700

    Switch Canvas connect flow to Chrome Web Store install

---
+++ app-config.js
@@ -0,0 +1,4 @@
+// Update this after each Chrome Web Store publish if the URL changes.
+window.BP_CONFIG = {
+  chromeWebStoreUrl: 'https://chromewebstore.google.com/detail/REPLACE_WITH_EXTENSION_ID'
+};
